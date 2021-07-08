a2c2127f03bdb501364de2491e56112d8628ccbb
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-08 18:01:22 +0530

    Add API setup and basic utils

--- src/modules/launches/types.ts
+++ src/modules/launches/types.ts
@@ -7,3 +7,15 @@ export type FilterType = {
     end: string;
   };
 };
+
+export type LaunchQueryType = {
+  upcoming?: boolean;
+  past?: boolean;
+  status?: string;
+  date_utc?: DateRange;
+};
+
+type DateRange = {
+  $gte?: string;
+  $lte?: string;
+};
--- src/modules/launches/utils/index.ts
+++ src/modules/launches/utils/index.ts
@@ -1,15 +1,17 @@
-import { FilterType } from "../types";
+import { FilterType, LaunchQueryType } from "../types";
 
 export function getQueryFromFilter(filter: FilterType) {
+  const query: LaunchQueryType = {};
+  if (filter.upcoming) query.upcoming = true;
+  if (filter.past) query.past = true;
+  if (filter.status) query.status = filter.status;
+  if (filter.date)
+    query.date_utc = {
+      $gte: filter.date.start,
+      $lte: filter.date.end,
+    };
+
   return {
-    query: {
-      upcoming: filter.upcoming,
-      past: filter.past,
-      status: filter.status,
-      date_utc: {
-        $gte: filter.date.start,
-        $lte: filter.date.end,
-      },
-    },
+    query,
   };
 }
