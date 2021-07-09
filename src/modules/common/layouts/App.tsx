e36de9eff17ea84973d65e948c2051799f9929e7
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-09 12:50:23 +0530

    create app layout with top bar

--- src/modules/common/layouts/App.tsx
+++ src/modules/common/layouts/App.tsx
@@ -1,9 +1,18 @@
 import * as React from "react";
+import { Flex, Container } from "@chakra-ui/react";
 
+import { Logo } from "assets/Logo";
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
 export default function AppLayout({ children }: AppLayoutProps) {
-  return <div>{children}</div>;
+  return (
+    <Flex direction="column" bg="white" height="100vh">
+      <Flex align="center" justify="center" bg="white" py="5" boxShadow="xs">
+        <Logo flexShrink={0} maxHeight="8" />
+      </Flex>
+      <Container maxW='container.lg'>{children}</Container>
+    </Flex>
+  );
 }
