86acacdaa39cf29f0aa3a241bfd439342ca35bf0
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-10 18:08:23 +0530

    add nitro-gray to chakra theme

--- src/theme.ts
+++ src/theme.ts
@@ -5,4 +5,12 @@ export const theme = extendTheme({
     heading: "Inter",
     body: "Inter",
   },
+  colors: {
+    nitroGray: {
+      100: "#F4F5F7",
+      200: "#E4E4E7",
+      600: "#4B5563",
+      800: "#1F2937",
+    },
+  },
 });
