d95e154c090cf7082ce7851aa5f50273d060da43
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-10 17:20:07 +0530

    add table styles

--- src/modules/common/components/table/index.tsx
+++ src/modules/common/components/table/index.tsx
@@ -1,24 +1,47 @@
 import * as React from "react";
 import { useTable } from "react-table";
-import { Table as ChakraTable, Thead, Tr, Tbody, Td } from "@chakra-ui/react";
+import {
+  Table as ChakraTable,
+  Thead,
+  Tr,
+  Tbody,
+  Td,
+  Th,
+} from "@chakra-ui/react";
 import { TableProperties } from "./types";
 
 export default function Table<T extends Record<string, unknown>>(
   props: React.PropsWithChildren<TableProperties<T>>
 ): React.ReactElement {
   const { columns, data } = props;
+
   const { getTableProps, getTableBodyProps, prepareRow, rows, headerGroups } =
     useTable<T>({
       columns,
       data,
     });
+
   return (
-    <ChakraTable my="8" borderWidth="1px" fontSize="sm" {...getTableProps()}>
-      <Thead bg="gray.50">
+    <ChakraTable
+      size="sm"
+      my="8"
+      borderWidth="1px"
+      variant="unstyled"
+      fontSize="sm"
+      {...getTableProps()}
+    >
+      <Thead bg="gray.50" h={8}>
         {headerGroups.map((headerGroup) => (
           <Tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              <Th
+                fontWeight={"medium"}
+                {...column.getHeaderProps()}
+                fontSize={12}
+                textTransform="capitalize"
+              >
+                {column.render("Header")}
+              </Th>
             ))}
           </Tr>
         ))}
@@ -29,7 +52,18 @@ export default function Table<T extends Record<string, unknown>>(
           return (
             <Tr {...row.getRowProps()}>
               {row.cells.map((cell) => {
-                return <Td {...cell.getCellProps()}>{cell.render("Cell")}</Td>;
+                return (
+                  <Td
+                    fontSize={12}
+                    fontWeight="normal"
+                    lineHeight={1}
+                    border="none"
+                    py={5}
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render("Cell")}
+                  </Td>
+                );
               })}
             </Tr>
           );
