b2578ba3e6e203384869b5e0b2ab06b6e817eb7f
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-10 17:17:37 +0530

    add Rocket type

--- src/modules/launches/types.ts
+++ src/modules/launches/types.ts
@@ -48,7 +48,7 @@ export type SpaceXApiResponse = {
   tbd?: Boolean;
   net?: Boolean;
   window?: number | null;
-  rocket?: string | null;
+  rocket?: Rocket | null;
   success?: Boolean | null;
   failures?: {
     time: number;
@@ -143,30 +143,121 @@ type Payload = {
   id: string;
 };
 
-
 type Launchpad = {
-    name: string | null;
-    full_name: string | null;
-    status:
-      | "active"
-      | "inactive"
-      | "unknown"
-      | "retired"
-      | "lost"
-      | "under construction";
+  name: string | null;
+  full_name: string | null;
+  status:
+    | "active"
+    | "inactive"
+    | "unknown"
+    | "retired"
+    | "lost"
+    | "under construction";
 
-    locality: string | null;
-    region: string | null;
-    timezone: string | null;
-    latitude: number | null;
-    longitude: number | null;
-    launch_attempts: number;
-    launch_successes: number;
-    rockets: string[] | null;
-    launches: string[] | null;
-    images: {
-      large: string[] | null;
-    };
-    details: string | null;
-    id: string | null;
+  locality: string | null;
+  region: string | null;
+  timezone: string | null;
+  latitude: number | null;
+  longitude: number | null;
+  launch_attempts: number;
+  launch_successes: number;
+  rockets: string[] | null;
+  launches: string[] | null;
+  images: {
+    large: string[] | null;
   };
+  details: string | null;
+  id: string | null;
+};
+
+type Rocket = {
+  name: string;
+  type: string;
+  active: Boolean;
+  stages: number;
+  boosters: number;
+  cost_per_launch: number;
+  success_rate_pct: number;
+  first_flight: string;
+  country: string;
+  company: string;
+  height: {
+    meters: number;
+    feet: number;
+  };
+  diameter: {
+    meters: number;
+    feet: number;
+  };
+  mass: {
+    kg: number;
+    lb: number;
+  };
+  payload_weights: any[];
+  first_stage: {
+    reusable: Boolean;
+    engines: number;
+    fuel_amount_tons: number;
+    burn_time_sec: number;
+    thrust_sea_level: {
+      kN: number;
+      lbf: number;
+    };
+    thrust_vacuum: {
+      kN: number;
+      lbf: number;
+    };
+  };
+  second_stage: {
+    reusable: Boolean;
+    engines: number;
+    fuel_amount_tons: number;
+    burn_time_sec: number;
+    thrust: {
+      kN: number;
+      lbf: number;
+    };
+    payloads: {
+      option_1: string;
+      composite_fairing: {
+        height: {
+          meters: number;
+          feet: number;
+        };
+        diameter: {
+          meters: number;
+          feet: number;
+        };
+      };
+    };
+  };
+  engines: {
+    number: number;
+    type: string;
+    version: string;
+    layout: string;
+    isp: {
+      sea_level: number;
+      vacuum: number;
+    };
+    engine_loss_max: number;
+    propellant_1: string;
+    propellant_2: string;
+    thrust_sea_level: {
+      kN: number;
+      lbf: number;
+    };
+    thrust_vacuum: {
+      kN: number;
+      lbf: number;
+    };
+    thrust_to_weight: number;
+  };
+  landing_legs: {
+    number: number;
+    material: any;
+  };
+  flickr_images: string[];
+  wikipedia: string;
+  description: string;
+};
