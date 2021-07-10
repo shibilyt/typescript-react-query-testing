430ce604ee561dd4fe539d286bc92dcb360f06ef
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-10 17:11:26 +0530

    add Inter font-family

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import { App } from "./App";
+import { theme } from "./theme";
+
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
+
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/400.css";
 
 const queryClient = new QueryClient();
 
