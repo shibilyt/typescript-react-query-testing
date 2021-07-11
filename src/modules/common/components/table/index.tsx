75fb81da88046c559d459463d4ba3c7d2ed69f11
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-11 13:39:20 +0530

    style table with theme colors and borders

--- src/modules/common/components/table/index.tsx
+++ src/modules/common/components/table/index.tsx
@@ -7,6 +7,8 @@ import {
   Tbody,
   Td,
   Th,
+  chakra,
+  Box,
 } from "@chakra-ui/react";
 import { TableProperties } from "./types";
 
@@ -22,53 +24,68 @@ export default function Table<T extends Record<string, unknown>>(
     });
 
   return (
-    <ChakraTable
-      size="sm"
-      my="8"
-      borderWidth="1px"
-      variant="unstyled"
-      fontSize="sm"
-      {...getTableProps()}
+    <Box
+      mt={8}
+      border="1px solid"
+      borderColor="nitroGray.200"
+      borderRadius="md"
+      overflow="hidden"
     >
-      <Thead bg="gray.50" h={8}>
-        {headerGroups.map((headerGroup) => (
-          <Tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <Th
-                fontWeight={"medium"}
-                {...column.getHeaderProps()}
-                fontSize={12}
-                textTransform="capitalize"
-              >
-                {column.render("Header")}
-              </Th>
-            ))}
-          </Tr>
-        ))}
-      </Thead>
-      <Tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <Tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <Td
-                    fontSize={12}
-                    fontWeight="normal"
-                    lineHeight={1}
-                    border="none"
-                    py={5}
-                    {...cell.getCellProps()}
-                  >
-                    {cell.render("Cell")}
-                  </Td>
-                );
-              })}
+      <ChakraTable
+        size="sm"
+        fontSize="sm"
+        variant="unstyled"
+        boxShadow="base"
+        {...getTableProps()}
+      >
+        <Thead bg="nitroGray.100" h={8} color="nitroGray.600">
+          {headerGroups.map((headerGroup) => (
+            <Tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <Th
+                  fontWeight={"medium"}
+                  {...column.getHeaderProps()}
+                  fontSize={12}
+                  textTransform="capitalize"
+                >
+                  {column.render("Header")}
+                </Th>
+              ))}
             </Tr>
-          );
-        })}
-      </Tbody>
-    </ChakraTable>
+          ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {rows.length ? (
+            rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <Tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <Td
+                        fontSize={12}
+                        fontWeight="normal"
+                        lineHeight={1}
+                        border="none"
+                        py={5}
+                        color={"nitroGray.800"}
+                        {...cell.getCellProps()}
+                      >
+                        {cell.render("Cell")}
+                      </Td>
+                    );
+                  })}
+                </Tr>
+              );
+            })
+          ) : (
+            <chakra.div
+              minHeight="636px"
+              data-testid="empty-table"
+            ></chakra.div>
+          )}
+        </Tbody>
+      </ChakraTable>
+    </Box>
   );
 }
