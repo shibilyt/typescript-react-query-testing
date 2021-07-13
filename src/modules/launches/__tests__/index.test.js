d8e170b475f33671718546eec6c477c9aed8fa2e
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-13 19:59:15 +0530

    update launches tests add integration tests for filters

--- src/modules/launches/__tests__/index.test.js
+++ src/modules/launches/__tests__/index.test.js
@@ -1,4 +1,5 @@
 import { screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { rest } from "msw";
 import { server } from "test/mocks/server";
 import { render, renderHook } from "test/test-utils";
@@ -11,13 +12,31 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test("useGetLaunches returns launch data with no initial filter", async () => {
-  const filter = {};
-  const { result, waitForNextUpdate } = renderHook(() =>
+test("useGetLaunches returns launch data with filters", async () => {
+  let filter = "all";
+  const { result, waitForNextUpdate, rerender } = renderHook(() =>
     useGetLaunches({ filter })
   );
   await waitForNextUpdate();
-  expect(result.current.data).toBeTruthy();
+  expect(result.current.data.docs).toHaveLength(12);
+  filter = "upcoming";
+  rerender();
+  await waitFor(() => {
+    expect(result.current.status).toEqual("success");
+  });
+  expect(result.current.data.docs).toHaveLength(9);
+  filter = "success";
+  rerender();
+  await waitFor(() => {
+    expect(result.current.status).toEqual("success");
+  });
+  expect(result.current.data.docs).toHaveLength(0);
+  filter = "failed";
+  rerender();
+  await waitFor(() => {
+    expect(result.current.status).toEqual("success");
+  });
+  expect(result.current.data.docs).toHaveLength(3);
 });
 
 test("Launches table displays data from the response", async () => {
@@ -49,4 +68,52 @@ test("empty response gives an empty div with table headers", async () => {
   });
   expect(screen.queryByTestId("empty-table")).toBeInTheDocument();
   expect(screen.getAllByRole("row")).toHaveLength(1);
+});
+
+test("upcoming filter filters out upcoming launches", async () => {
+  render(<Launches />);
+  const selectButton = screen.getByRole("button");
+  expect(screen.queryByTestId("loading")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.queryByTestId("loading")).not.toBeInTheDocument();
+  });
+  userEvent.click(selectButton);
+  userEvent.click(screen.getByRole("option", { name: /upcoming/i }));
+  expect(screen.queryByTestId("loading")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.queryByTestId("loading")).not.toBeInTheDocument();
+  });
+  expect(screen.getAllByRole("row")).toHaveLength(10);
+});
+
+test("success filter filters out successful launches", async () => {
+  render(<Launches />);
+  const selectButton = screen.getByRole("button");
+  expect(screen.queryByTestId("loading")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.queryByTestId("loading")).not.toBeInTheDocument();
+  });
+  userEvent.click(selectButton);
+  userEvent.click(screen.getByRole("option", { name: /success/i }));
+  expect(screen.queryByTestId("loading")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.queryByTestId("loading")).not.toBeInTheDocument();
+  });
+  expect(screen.getAllByRole("row")).toHaveLength(1);
+});
+
+test("fail filter filters out failed launches", async () => {
+  render(<Launches />);
+  const selectButton = screen.getByRole("button");
+  expect(screen.queryByTestId("loading")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.queryByTestId("loading")).not.toBeInTheDocument();
+  });
+  userEvent.click(selectButton);
+  userEvent.click(screen.getByRole("option", { name: /failed/i }));
+  expect(screen.queryByTestId("loading")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.queryByTestId("loading")).not.toBeInTheDocument();
+  });
+  expect(screen.getAllByRole("row")).toHaveLength(4);
 });
