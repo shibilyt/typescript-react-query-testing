64b6abb1037c166a6ad2166de1d0b22b2776dc17
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-14 12:00:37 +0530

    fix query body type in the mock handlers

--- src/test/mocks/handlers.ts
+++ src/test/mocks/handlers.ts
@@ -2,10 +2,10 @@ import { rest } from "msw";
 import { launchesResolver } from "./resolvers";
 import { mockData } from "./_data";
 import { launchesEndPoints } from "modules/launches/queries";
-import { FilterType } from "modules/launches/types";
+import { LaunchQueryType } from "modules/launches/types";
 
 export const handlers = [
-  rest.post<FilterType>(
+  rest.post<{ query: LaunchQueryType; options: any }>(
     process.env.REACT_APP_BASE_URL + launchesEndPoints.getLaunches,
     launchesResolver(mockData)
   ),
