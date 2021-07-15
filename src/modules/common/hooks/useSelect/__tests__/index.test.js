5d0c3fc4df1a679f68d835e357e34d709b61e63d
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-15 12:49:18 +0530

    add test for disabled options support to useSelect

--- src/modules/common/hooks/useSelect/__tests__/index.test.js
+++ src/modules/common/hooks/useSelect/__tests__/index.test.js
@@ -20,45 +20,79 @@ const options = [
     value: "opt4",
   },
 ];
-test("useSelect returns proper value when selecting an option", () => {
-  const { result } = renderHook(() => useSelect({ options }));
 
-  expect(result.current.value).toBeUndefined();
-  act(() => {
-    result.current.selectIndex(1);
+describe("useSelect", () => {
+  test("returns proper value when selecting an option", () => {
+    const { result } = renderHook(() => useSelect({ options }));
+
+    expect(result.current.value).toBeUndefined();
+    act(() => {
+      result.current.selectIndex(1);
+    });
+    expect(result.current.value).toEqual("opt2");
   });
-  expect(result.current.value).toEqual("opt2");
-});
 
-test("controlled Value sets value properly", () => {
-  let value = "test Value";
-  const { result, rerender } = renderHook(() =>
-    useSelect({ options, value, onChange: () => {} })
-  );
+  test("controlled Value sets value properly", () => {
+    let value = "test Value";
+    const { result, rerender } = renderHook(() =>
+      useSelect({ options, value, onChange: () => {} })
+    );
 
-  expect(result.current.value).toEqual(value);
+    expect(result.current.value).toEqual(value);
 
-  value = "updated value";
-  rerender();
-  expect(result.current.value).toEqual(value);
-});
-
-test("controlled select receives selectedOption in onChange", () => {
-  let value = "test Value";
-  const onChange = jest.fn((option) => {
-    value = option.value;
+    value = "updated value";
+    rerender();
+    expect(result.current.value).toEqual(value);
   });
-  const { result } = renderHook(() => useSelect({ options, value, onChange }));
 
-  expect(result.current.value).toEqual(value);
+  test("controlled select receives selectedOption in onChange", () => {
+    let value = "test Value";
+    const onChange = jest.fn((option) => {
+      value = option.value;
+    });
+    const { result } = renderHook(() =>
+      useSelect({ options, value, onChange })
+    );
 
-  act(() => {
-    result.current.selectIndex(1);
+    expect(result.current.value).toEqual(value);
+
+    act(() => {
+      result.current.selectIndex(1);
+    });
+    const option = options[1];
+
+    expect(result.current.selectedOption).toEqual(option);
+
+    expect(onChange).toHaveBeenCalledWith(option);
+    expect(onChange).toHaveBeenCalledTimes(1);
   });
-  const option = options[1];
 
-  expect(result.current.selectedOption).toEqual(option);
+  test("should not allow disable options to be selected", () => {
+    let optionsWithDisabled = [
+      ...options,
+      { label: "disabled option", value: "disabled", disabled: true },
+    ];
+    let value = "opt2";
+    const onChange = jest.fn((option) => {
+      value = option.value;
+    });
+    const { result } = renderHook(() =>
+      useSelect({ options: optionsWithDisabled, value, onChange })
+    );
 
-  expect(onChange).toHaveBeenCalledWith(option);
-  expect(onChange).toHaveBeenCalledTimes(1);
+    expect(result.current.value).toEqual(value);
+    act(() => {
+      result.current.setOpen(true);
+    });
+
+    act(() => {
+      result.current.selectIndex(optionsWithDisabled.length - 1);
+    });
+
+    //   should not close when clicking on disabled option
+    expect(result.current.isOpen).toEqual(true);
+
+    //   shouldn't change value on click disabled option
+    expect(result.current.value).toEqual(value);
+  });
 });
