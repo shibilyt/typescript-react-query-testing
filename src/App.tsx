0eb7fee15345c6410441c4246b54fd9985c125a1
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-20 14:10:08 +0530

    cleanup index and app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,22 @@
 import * as React from "react";
-import AppRouter from "modules/common/router";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-export const App = () => <AppRouter />;
+import AppRouter from "modules/common/router";
+import { theme } from "./theme";
+
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/400.css";
+
+const queryClient = new QueryClient();
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AppRouter />
+      </Router>
+    </QueryClientProvider>
+  </ChakraProvider>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,11 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "react-query";
 
 import { App } from "./App";
-import { theme } from "./theme";
-
-import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorker";
-
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/400.css";
-
-const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <App />
-        </Router>
-      </QueryClientProvider>
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
