1dc201538ec8165bca36cbdd6fcf820c38f8d7d1
Mohammed Jumail Shibily <3939547+shibilyt@users.noreply.github.com>
2021-07-22 12:41:27 +0530

    update date filter to reflect utils changes

--- src/modules/launches/components/dateFilter/index.tsx
+++ src/modules/launches/components/dateFilter/index.tsx
@@ -17,23 +17,30 @@ import {
   MonthType,
   START_DATE,
 } from "@datepicker-react/hooks";
-import addDays from "date-fns/addDays";
 import addMonths from "date-fns/addMonths";
 import isSameMonth from "date-fns/isSameMonth";
-import addYears from "date-fns/addYears";
 import { useImmer } from "use-immer";
 
 import Datepicker, { DateState } from "modules/common/components/datePicker";
 import { Calendar as CalendarIcon } from "assets/Calendar";
 
-import { DateFilterType, filterStatuses } from "./types";
+import { DateFilterType, filterRanges } from "./types";
 import format from "date-fns/format";
+import {
+  getActiveMonths,
+  getPastMonth,
+  getPastSixMonths,
+  getPastThreeMonths,
+  getPastTwoYears,
+  getPastWeek,
+  getPastYear,
+} from "modules/launches/utils";
 
 interface DateFilterProps {
   dateFilter: DateFilterType;
-  setDateFilter: (data: DateFilterType) => void;
-  filterRange: string;
-  setFilterRange: React.Dispatch<React.SetStateAction<string>>;
+  setDateFilter?: (data: DateFilterType) => void;
+  filterRange: string | null;
+  setFilterRange: (range: string) => void;
 }
 
 export default function DateFilter({
@@ -47,41 +54,46 @@ export default function DateFilter({
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
+  const prevRange = React.useRef(filterRange);
+
   React.useEffect(() => {
-    switch (filterRange) {
-      case filterStatuses.pastWeek: {
-        handlePastWeekClick();
-        break;
+    if (filterRange !== prevRange.current) {
+      prevRange.current = filterRange;
+      switch (filterRange) {
+        case filterRanges.pastWeek: {
+          handlePastWeekClick();
+          break;
+        }
+        case filterRanges.pastMonth: {
+          handlePastMonthClick();
+          break;
+        }
+        case filterRanges.pastThreeMonths: {
+          handlePastThreeMonthsClick();
+          break;
+        }
+        case filterRanges.pastSixMonths: {
+          handlePastSixMonthsClick();
+          break;
+        }
+        case filterRanges.pastYear: {
+          handlePastYearClick();
+          break;
+        }
+        case filterRanges.pastTwoYear: {
+          handlePastTwoYearClick();
+          break;
+        }
+        default:
+          break;
       }
-      case filterStatuses.pastMonth: {
-        handlePastMonthClick();
-        break;
-      }
-      case filterStatuses.pastThreeMonths: {
-        handlePastThreeMonthsClick();
-        break;
-      }
-      case filterStatuses.pastSixMonths: {
-        handlePastSixMonthsClick();
-        break;
-      }
-      case filterStatuses.pastYear: {
-        handlePastYearClick();
-        break;
-      }
-      case filterStatuses.pastTwoYear: {
-        handlePastTwoYearClick();
-        break;
-      }
-      default:
-        break;
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filterRange]);
 
   const handleDateStateChange = ({ focusedInput, ...data }: DateState) => {
     setFocusedInput(focusedInput);
-    setDateFilter(data);
+    setDateFilter?.(data);
     if (data.startDate)
       setFilterRange(
         `${format(data.startDate, "d MMM yyyy")} - ${
@@ -90,94 +102,73 @@ export default function DateFilter({
       );
   };
 
-  const [activeMonths, setActiveMonths] = useImmer<MonthType[]>(() =>
-    getInitialMonths(2, null)
-  );
+  const [activeMonths, setActiveMonths] = useImmer<MonthType[]>(() => {
+    if (filterRange) {
+      return getActiveMonths(filterRange);
+    }
+    return getInitialMonths(2, null);
+  });
 
   const handlePastWeekClick = () => {
-    const endDate = new Date();
-    const startDate = addDays(endDate, -7);
-    setDateFilter({
-      startDate,
-      endDate,
-    });
+    const pastWeek = getPastWeek();
+    setDateFilter?.(pastWeek);
     setActiveMonths((draft) => {
-      draft[0] = getDateMonthAndYear(startDate);
-      draft[1] = isSameMonth(startDate, endDate)
-        ? getDateMonthAndYear(addMonths(endDate, 1))
-        : getDateMonthAndYear(endDate);
+      draft[0] = getDateMonthAndYear(pastWeek.startDate);
+      draft[1] = isSameMonth(pastWeek.startDate, pastWeek.endDate)
+        ? getDateMonthAndYear(addMonths(pastWeek.endDate, 1))
+        : getDateMonthAndYear(pastWeek.endDate);
     });
-    setFilterRange(filterStatuses.pastWeek);
+    setFilterRange(filterRanges.pastWeek);
   };
 
   const handlePastMonthClick = () => {
-    const endDate = new Date();
-    const startDate = addMonths(endDate, -1);
-    setDateFilter({
-      startDate,
-      endDate,
-    });
+    const pastMonth = getPastMonth();
+    setDateFilter?.(pastMonth);
     setActiveMonths((draft) => {
-      draft[0] = getDateMonthAndYear(startDate);
-      draft[1] = getDateMonthAndYear(endDate);
+      draft[0] = getDateMonthAndYear(pastMonth.startDate);
+      draft[1] = getDateMonthAndYear(pastMonth.endDate);
     });
-    setFilterRange(filterStatuses.pastMonth);
+    setFilterRange(filterRanges.pastMonth);
   };
 
   const handlePastThreeMonthsClick = () => {
-    const endDate = new Date();
-    const startDate = addMonths(endDate, -3);
-    setDateFilter({
-      startDate,
-      endDate,
-    });
+    const pastThreeMonths = getPastThreeMonths();
+    setDateFilter?.(pastThreeMonths);
     setActiveMonths((draft) => {
-      draft[0] = getDateMonthAndYear(startDate);
-      draft[1] = getDateMonthAndYear(endDate);
+      draft[0] = getDateMonthAndYear(pastThreeMonths.startDate);
+      draft[1] = getDateMonthAndYear(pastThreeMonths.endDate);
     });
-    setFilterRange(filterStatuses.pastThreeMonths);
+    setFilterRange(filterRanges.pastThreeMonths);
   };
 
   const handlePastSixMonthsClick = () => {
-    const endDate = new Date();
-    const startDate = addMonths(endDate, -6);
-    setDateFilter({
-      startDate,
-      endDate,
-    });
+    const pastSixMonths = getPastSixMonths();
+    setDateFilter?.(pastSixMonths);
     setActiveMonths((draft) => {
-      draft[0] = getDateMonthAndYear(startDate);
-      draft[1] = getDateMonthAndYear(endDate);
+      draft[0] = getDateMonthAndYear(pastSixMonths.startDate);
+      draft[1] = getDateMonthAndYear(pastSixMonths.endDate);
     });
-    setFilterRange(filterStatuses.pastSixMonths);
+    setFilterRange(filterRanges.pastSixMonths);
   };
 
   const handlePastYearClick = () => {
-    const endDate = new Date();
-    const startDate = addYears(endDate, -1);
-    setDateFilter({
-      startDate,
-      endDate,
-    });
+    const pastYear = getPastYear();
+    setDateFilter?.(pastYear);
     setActiveMonths((draft) => {
-      draft[0] = getDateMonthAndYear(startDate);
-      draft[1] = getDateMonthAndYear(endDate);
+      draft[0] = getDateMonthAndYear(pastYear.startDate);
+      draft[1] = getDateMonthAndYear(pastYear.endDate);
     });
-    setFilterRange(filterStatuses.pastYear);
+    setFilterRange(filterRanges.pastYear);
   };
 
   const handlePastTwoYearClick = () => {
-    const endDate = new Date();
-    const startDate = addYears(endDate, -2);
-    setDateFilter({
-      startDate,
-      endDate,
-    });
+    const pastTwoYears = getPastTwoYears();
+    setDateFilter?.(pastTwoYears);
     setActiveMonths((draft) => {
-      draft[0] = getDateMonthAndYear(startDate);
-      draft[1] = getDateMonthAndYear(endDate);
+      draft[0] = getDateMonthAndYear(pastTwoYears.startDate);
+      draft[1] = getDateMonthAndYear(pastTwoYears.endDate);
     });
-    setFilterRange(filterStatuses.pastTwoYear);
+    setFilterRange(filterRanges.pastTwoYear);
   };
 
   return (
@@ -212,7 +203,7 @@ export default function DateFilter({
               <chakra.button
                 my={1}
                 fontWeight={
-                  filterRange === filterStatuses.pastWeek ? "600" : "500"
+                  filterRange === filterRanges.pastWeek ? "600" : "500"
                 }
                 onClick={handlePastWeekClick}
               >
@@ -221,7 +212,7 @@ export default function DateFilter({
               <chakra.button
                 my={1}
                 fontWeight={
-                  filterRange === filterStatuses.pastMonth ? "600" : "500"
+                  filterRange === filterRanges.pastMonth ? "600" : "500"
                 }
                 onClick={handlePastMonthClick}
               >
@@ -230,7 +221,7 @@ export default function DateFilter({
               <chakra.button
                 my={1}
                 fontWeight={
-                  filterRange === filterStatuses.pastThreeMonths ? "600" : "500"
+                  filterRange === filterRanges.pastThreeMonths ? "600" : "500"
                 }
                 onClick={handlePastThreeMonthsClick}
               >
@@ -239,7 +230,7 @@ export default function DateFilter({
               <chakra.button
                 my={1}
                 fontWeight={
-                  filterRange === filterStatuses.pastSixMonths ? "600" : "500"
+                  filterRange === filterRanges.pastSixMonths ? "600" : "500"
                 }
                 onClick={handlePastSixMonthsClick}
               >
@@ -248,7 +239,7 @@ export default function DateFilter({
               <chakra.button
                 my={1}
                 fontWeight={
-                  filterRange === filterStatuses.pastYear ? "600" : "500"
+                  filterRange === filterRanges.pastYear ? "600" : "500"
                 }
                 onClick={handlePastYearClick}
               >
@@ -257,7 +248,7 @@ export default function DateFilter({
               <chakra.button
                 my={1}
                 fontWeight={
-                  filterRange === filterStatuses.pastTwoYear ? "600" : "500"
+                  filterRange === filterRanges.pastTwoYear ? "600" : "500"
                 }
                 onClick={handlePastTwoYearClick}
               >
