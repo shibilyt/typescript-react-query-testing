e7c9178862675389b9d6b959325b0c1f802644f6
Mohammed <mohammed.shibily@byjus.com>
2021-10-08 08:35:08 +0530

    Merge pull request #37 from Victordennis/36-bugfix/pagination-in-url
    36 bugfix/pagination in url

--- bigbinary-internal.md
+++
@@ -1,3 +0,0 @@
-This information is for the BigBinary Hiring Manager and not for the candidate. 
-The candidate will not able to access the link and they should not ask for access. 
-https://trello.com/c/1iIlmosq
--- src/modules/launches/utils/index.ts
+++ src/modules/launches/utils/index.ts
@@ -1,3 +1,8 @@
+import { getDateMonthAndYear } from "@datepicker-react/hooks";
+import addDays from "date-fns/addDays";
+import addMonths from "date-fns/addMonths";
+import addYears from "date-fns/addYears";
+import { filterRanges } from "../components/dateFilter/types";
 import {
   FilterType,
   filterStates,
@@ -32,4 +37,110 @@ export function getStatusOfLaunch(launchData: SpaceXApiResponse) {
 export function getFormatString(status: string) {
   if (status === "Failed") return "dd MMMM yyyy 'at' HH:mm";
   return "dd MMMM yyyy HH:mm";
+}
+
+export function getPastWeek() {
+  const endDate = new Date();
+  return {
+    startDate: addDays(endDate, -7),
+    endDate,
+  };
+}
+
+export function getPastMonth() {
+  const endDate = new Date();
+  return {
+    startDate: addMonths(endDate, -1),
+    endDate,
+  };
+}
+
+export function getPastThreeMonths() {
+  const endDate = new Date();
+  return {
+    startDate: addMonths(endDate, -3),
+    endDate,
+  };
+}
+
+export function getPastSixMonths() {
+  const endDate = new Date();
+  return {
+    startDate: addMonths(endDate, -6),
+    endDate,
+  };
+}
+
+export function getPastYear() {
+  const endDate = new Date();
+  return {
+    startDate: addYears(endDate, -1),
+    endDate,
+  };
+}
+
+export function getPastTwoYears() {
+  const endDate = new Date();
+  return {
+    startDate: addYears(endDate, -2),
+    endDate,
+  };
+}
+
+export function getDates(range: string) {
+  switch (range) {
+    case filterRanges.pastWeek:
+      return getPastWeek();
+    case filterRanges.pastMonth:
+      return getPastMonth();
+    case filterRanges.pastThreeMonths:
+      return getPastThreeMonths();
+    case filterRanges.pastSixMonths:
+      return getPastSixMonths();
+    case filterRanges.pastYear:
+      return getPastYear();
+    case filterRanges.pastTwoYear:
+      return getPastTwoYears();
+    default: {
+      const [start, end] = range.split("-").map((d) => d.trim());
+
+      if (end == null || end === "Upcoming") {
+        return {
+          startDate: new Date(start),
+          endDate: null,
+        };
+      }
+      return {
+        startDate: new Date(start),
+        endDate: new Date(end),
+      };
+    }
+  }
+}
+
+export function getActiveMonths(filterRange: string) {
+  switch (filterRange) {
+    case filterRanges.pastWeek:
+    case filterRanges.pastMonth:
+    case filterRanges.pastThreeMonths:
+    case filterRanges.pastSixMonths:
+    case filterRanges.pastYear:
+    case filterRanges.pastTwoYear: {
+      const dates = getDates(filterRange);
+      return [
+        getDateMonthAndYear(dates.startDate as Date),
+        getDateMonthAndYear(dates.endDate as Date),
+      ];
+    }
+    default: {
+      const dates = getDates(filterRange);
+      if (!dates.endDate) {
+        return [
+          getDateMonthAndYear(dates.startDate),
+          getDateMonthAndYear(addMonths(new Date(), 1)),
+        ];
+      }
+      return [getDateMonthAndYear(dates.startDate)];
+    }
+  }
 }
